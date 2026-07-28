a008eca0d25b23d7be5eaf65e1f3f60cf9b52f58
jaswanth-geddam <jaswanth.geddam.lead@gmail.com>
2026-07-28 15:25:17 +0530

    refactor: consolidate react-practice into npm workspaces

--- run.bat
+++ run.bat
@@ -2,88 +2,90 @@
 cd /d "%~dp0"
 
 echo ============================================
-echo  React Practice - Sub Projects
+echo  React Practice - Workspace Mode
+echo  (single install at root level)
 echo ============================================
 echo.
 
 :MENU
-echo Available projects:
-echo  1. action-reducer
-echo  2. auth-context
-echo  3. c4
-echo  4. context
-echo  5. e2
-echo  6. employee-app
-echo  7. falcon
-echo  8. infinity-scroll
-echo  9. jaswant
-echo 10. music
-echo 11. react-1
-echo 12. rec-comments
-echo 13. recursion-comments
-echo 14. redux
-echo 15. routing-app
-echo 16. routing-assignment
-echo 17. thunk
+echo Available commands:
 echo.
-echo  a. Install all
-echo  l. List with status
+echo  i. Install all (single npm install at root)
+echo  r. Run a sub-project
+echo  f. Format code
+echo  l. List projects
 echo  0. Exit
 echo.
 set /p r=Choose: 
 
 if "%r%"=="0" exit /b
-if "%r%"=="a" goto INSTALL_ALL
-if "%r%"=="l" goto LIST
+if /i "%r%"=="i" goto INSTALL
+if /i "%r%"=="r" goto RUN
+if /i "%r%"=="f" goto FORMAT
+if /i "%r%"=="l" goto LIST
+goto MENU
 
-set idx=0
+:INSTALL
+echo Installing...
+call npm install
+echo Done!
+pause
+goto MENU
+
+:RUN
+cls
+echo ============================================
+echo  Select a project to start
+echo ============================================
+echo.
+echo  1.  action-reducer
+echo  2.  auth-context
+echo  3.  c4
+echo  4.  context
+echo  5.  e2
+echo  6.  employee-app/employee
+echo  7.  falcon
+echo  8.  jaswant
+echo  9.  music
+echo 10.  react-1
+echo 11.  recursion-comments
+echo 12.  redux
+echo 13.  routing-app
+echo 14.  routing-assignment
+echo 15.  thunk
+echo  0.  Back
+echo.
+set /p r=Choose: 
+
+if "%r%"=="0" goto MENU
+
 set "dirs[1]=action-reducer"
 set "dirs[2]=auth-context"
 set "dirs[3]=c4"
 set "dirs[4]=context"
 set "dirs[5]=e2"
-set "dirs[6]=employee-app"
+set "dirs[6]=employee-app\employee"
 set "dirs[7]=falcon"
-set "dirs[8]=infinity-scroll"
-set "dirs[9]=jaswant"
-set "dirs[10]=music"
-set "dirs[11]=react-1"
-set "dirs[12]=rec-comments"
-set "dirs[13]=recursion-comments"
-set "dirs[14]=redux"
-set "dirs[15]=routing-app"
-set "dirs[16]=routing-assignment"
-set "dirs[17]=thunk"
+set "dirs[8]=jaswant"
+set "dirs[9]=music"
+set "dirs[10]=react-1"
+set "dirs[11]=recursion-comments"
+set "dirs[12]=redux"
+set "dirs[13]=routing-app"
+set "dirs[14]=routing-assignment"
+set "dirs[15]=thunk"
 
-call :RUN_PROJECT %r%
-goto MENU
-
-:RUN_PROJECT
 setlocal enabledelayedexpansion
-set "d=!dirs[%1]!"
-if "!d!"=="" goto :EOF
-echo.
-echo === Installing !d! ===
-cd /d "%~dp0!d!"
-if exist package.json (
-  call npm install 2>nul
-  echo === Starting !d! ===
-  start "react-!d!" cmd /c "cd /d "%~dp0!d!" && npm start"
-) else (
-  echo No package.json found
-)
-cd /d "%~dp0"
+set "d=!dirs[%r%]!"
+if "!d!"=="" goto RUN
+echo Starting !d!...
+start "react-!d:\=!" cmd /c "cd /d "%~dp0!d!" && npm start"
 endlocal
-goto :EOF
+goto RUN
 
-:INSTALL_ALL
-echo === Installing all sub-projects ===
-for /d %%d in (*) do (
-  if exist "%%d\package.json" (
-    echo Installing %%d...
-    cd %%d && call npm install && cd ..
-  )
-)
+:FORMAT
+echo Formatting all files...
+call npm run format
 echo Done!
 pause
 goto MENU
@@ -91,7 +93,7 @@ goto MENU
 :LIST
 echo.
 for /d %%d in (*) do (
-  if exist "%%d\package.json" (echo [OK]  %%d) else (echo [--]  %%d)
+  if exist "%%d\package.json" (echo [OK]  %%d)
 )
 echo.
 pause
